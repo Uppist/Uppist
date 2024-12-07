ae381686e5633a5c0051583095208870ecb7465f
olaniyan18 <olaniyanfeyikemi@gmail.com>
2024-12-07 17:42:03 +0100

    New headove

--- src/components/Headove/headoveSection.jsx
+++ src/components/Headove/headoveSection.jsx
@@ -3,7 +3,6 @@
 import React from "react";
 import NavBar from "../Aboutus/NavBar";
 import Footer from "../Aboutus/Footer";
-import Headove from "./headove";
 import Headove from "./Headove";
 
 export default function HeadoveSection() {
